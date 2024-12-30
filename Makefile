c3638e031fcf1abb029183677ab4d62577a08bf9
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-12-30 17:15:05 -0300

    Add glm, ImGui, and Sol2 libraries to the project
    Integrate glm and Sol2 into the main.cpp file for vector math and Lua scripting support. Update the Makefile to include SDL2_mixer and adjust include paths for library dependencies. These changes enhance the project's flexibility and functionality.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	clang++ -Wall -std=c++17 src/*.cpp -lSDL2  -lSDL2_ttf -lSDL2_image -llua -I/opt/homebrew/include -L/opt/homebrew/lib -o gameengine;
+	clang++ -Wall -std=c++17 src/*.cpp -lSDL2  -lSDL2_ttf -lSDL2_image -lSDL2_mixer -llua -I"./libs" -I/opt/homebrew/include -L/opt/homebrew/lib -o gameengine;
 
 run:
 	./gameengine
--- src/Main.cpp
+++ src/Main.cpp
@@ -3,9 +3,15 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main() {
+  sol::state lua;
+  lua.open_libraries(sol::lib::base);
   SDL_Init(SDL_INIT_EVERYTHING);
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+  glm::vec2 velocity = glm::vec2(5.0, -2.0);
+
+  return 0;
 }
